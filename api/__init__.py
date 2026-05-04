908f6ba220cf911dcbf44c36cab24f9d10992ab6
peacemaker01 <givi@ICT.localdomain>
2026-05-04 17:21:03 +0100

    remove broken webhooks import from api/__init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -1,9 +1,8 @@
 # api/__init__.py
 from fastapi import FastAPI
-from api.webhooks import router as webhooks_router
 
 app = FastAPI(title="Aegis Webhook Receiver", version="1.0.0")
-app.include_router(webhooks_router)
+
 
 @app.get("/health")
 async def health():
